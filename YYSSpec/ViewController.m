9c0324ef3629eeb9dd291b9612811738c71fb50f
yueyaoshang <1759953211@qq.com>
2020-07-28 21:59:25 +0800

    添加字符串

--- YYSSpec/ViewController.m
+++ YYSSpec/ViewController.m
@@ -17,7 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    NSLog(@"123");
+    NSString *str = @"123";
+    NSLog(@"%@",str);
 }
 
 
